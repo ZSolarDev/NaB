ce44c6fcad2a95698aa96338ece33faa3af9e698
ZSolarDev <zsolardevofficial@gmail.com>
2026-05-11 23:29:06 -0400

    ..

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "/bare/",
+  bare: "https://nab-server-dpa4.onrender.com/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
